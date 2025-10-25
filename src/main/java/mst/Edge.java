ac35987f921d490c43945b9bb475f46d7e0ef2b6
Yerassyl <yerasil.yerkin@icloud.com>
2025-10-25 14:00:44 +0500

    Implement Graph and Edge classes

--- src/main/java/mst/Edge.java
+++ src/main/java/mst/Edge.java
@@ -1,4 +1,26 @@
 package mst;
 
-public class Edge {
+public class Edge implements Comparable<Edge> {
+    public final int u, v;
+    public final double w;
+
+    public Edge(int u, int v, double w) {
+        this.u = u;
+        this.v = v;
+        this.w = w;
+    }
+
+    public int other(int x) {
+        return x == u ? v : u;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return Double.compare(this.w, o.w);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d-%d:%.2f)", u, v, w);
+    }
 }
--- src/main/java/mst/Graph.java
+++ src/main/java/mst/Graph.java
@@ -1,4 +1,51 @@
 package mst;
 
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.*;
+
 public class Graph {
+    public final int n;
+    public final List<Edge> edges;
+
+    public Graph(int n, List<Edge> edges) {
+        this.n = n;
+        this.edges = edges;
+    }
+
+    public static List<Graph> loadFromFile(String path) throws IOException {
+        Gson gson = new Gson();
+        Reader r = new FileReader(path);
+        Type listType = new TypeToken<List<JsonObject>>(){}.getType();
+        List<JsonObject> arr = gson.fromJson(r, listType);
+        List<Graph> result = new ArrayList<>();
+        for (JsonObject jo : arr) {
+            int n = jo.get("n").getAsInt();
+            JsonArray eArr = jo.getAsJsonArray("edges");
+            List<Edge> edges = new ArrayList<>();
+            for (JsonElement e : eArr) {
+                JsonObject o = e.getAsJsonObject();
+                edges.add(new Edge(
+                        o.get("u").getAsInt(),
+                        o.get("v").getAsInt(),
+                        o.get("w").getAsDouble()
+                ));
+            }
+            result.add(new Graph(n, edges));
+        }
+        r.close();
+        return result;
+    }
+
+    public List<List<Edge>> adjacencyList() {
+        List<List<Edge>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (Edge e : edges) {
+            adj.get(e.u).add(e);
+            adj.get(e.v).add(e);
+        }
+        return adj;
+    }
 }
